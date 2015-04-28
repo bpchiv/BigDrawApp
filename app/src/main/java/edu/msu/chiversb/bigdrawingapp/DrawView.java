ab27c7b980f0a78d5b63cc5a2bfc9cf200cf96f9
Brian <bpchiv@gmail.com>
2015-04-28 18:23:41 -0400

    Initial Commit

---
+++ app/src/main/java/edu/msu/chiversb/bigdrawingapp/DrawView.java
@@ -0,0 +1,13 @@
+package edu.msu.chiversb.bigdrawingapp;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by bpchiv on 4/28/15.
+ */
+public class DrawView extends View{
+    public DrawView(Context context) {
+        super(context);
+    }
+}
