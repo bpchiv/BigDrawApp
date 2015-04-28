554acbd6f954d2fd82457efa00af097cf646f5cb
Brian <bpchiv@gmail.com>
2015-04-28 18:37:54 -0400

    More GPS Stuff
    I’m not sure why this didn’t commit with the last commit

--- app/src/main/java/edu/msu/chiversb/bigdrawingapp/MainActivity.java
+++ app/src/main/java/edu/msu/chiversb/bigdrawingapp/MainActivity.java
@@ -1,12 +1,31 @@
 package edu.msu.chiversb.bigdrawingapp;
 
+import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private LocationManager locationManager = null;
+
+    private double latitude = 0;
+    private double longitude = 0;
+    private boolean valid = false;
+
+    private ActiveListener activeListener = new ActiveListener();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +54,90 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    /**
+     * Set all user interface components to the current state
+     */
+    private void setUI() {
+        
+
+    }
+    /**
+     * Called when this application becomes foreground again.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        setUI();
+        registerListeners();
+    }
+
+    /**
+     * Called when this application is no longer the foreground application.
+     */
+    @Override
+    protected void onPause() {
+        unregisterListeners();
+        super.onPause();
+    }
+    private void unregisterListeners() {
+        locationManager.removeUpdates(activeListener);
+    }
+    private void registerListeners() {
+        unregisterListeners();
+
+        // Create a Criteria object
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setPowerRequirement(Criteria.POWER_HIGH);
+        criteria.setAltitudeRequired(true);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(false);
+
+        String bestAvailable = locationManager.getBestProvider(criteria, true);
+
+        if(bestAvailable != null) {
+            locationManager.requestLocationUpdates(bestAvailable, 500, 1, activeListener);
+            Location location = locationManager.getLastKnownLocation(bestAvailable);
+            onLocation(location);
+        }
+    }
+    private void onLocation(Location location) {
+        if(location == null) {
+            return;
+        }
+
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        valid = true;
+
+        setUI();
+    }
+
+
+    private class ActiveListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            onLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            registerListeners();
+        }
+
     }
 }
