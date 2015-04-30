995b8eb2617ba97f51c92557c88e8a748085bef5
patelas7 <patelas7@msu.edu>
2015-04-30 16:09:04 -0400

    draw and color

--- app/src/main/java/edu/msu/chiversb/bigdrawingapp/DrawView.java
+++ app/src/main/java/edu/msu/chiversb/bigdrawingapp/DrawView.java
@@ -19,9 +19,10 @@ public class DrawView extends View{
      * Paint to use when drawing the custom color hat
      */
     private Paint customPaint;
-
+    private double centerLong = 0;
+    private double centerLat = 0;
     private ArrayList<Location> locations = new ArrayList<Location>();
-    private int color = 0;
+    private ArrayList<Integer> colors = new ArrayList<Integer>();
     private boolean firstDraw = true;
 
     public DrawView(Context context, AttributeSet attrs) {
@@ -52,6 +53,7 @@ public class DrawView extends View{
 
     public void addLocation(Location location){
         locations.add(location);
+        colors.add(customPaint.getColor());
         Log.i("location size", String.valueOf(locations.size()));
         invalidate();
     }
@@ -61,9 +63,7 @@ public class DrawView extends View{
      * @param color pen color integer value
      */
     public void setColor(int color) {
-        this.color = color;
         customPaint.setColor(color);
-        invalidate();
     }
 
     /**
@@ -77,16 +77,21 @@ public class DrawView extends View{
         int width = canvas.getWidth();
         int height = canvas.getHeight();
 
-        double centerLong = locations.get(0).getLongitude();
-        double centerLat = locations.get(0).getLatitude();
-
-        double x = (width/360.0) * (180 + locations.get(0).getLongitude());
-        double y = (height/180.0) * (90 - locations.get(0).getLatitude());
+        if(firstDraw) {
+            centerLong = locations.get(0).getLongitude();
+            centerLat = locations.get(0).getLatitude();
+            firstDraw = false;
+        }
+        double x = (width/360.0) * (180 + centerLong);
+        double y = (height/180.0) * (90 - centerLat);
 
         double scaleX = x;
         double scaleY = y;
 
         for(int i = 1; i < locations.size(); i++) {
+
+            customPaint.setColor(colors.get(i));
+
             double dX = 0.0;
             double dY = 0.0;
 
@@ -108,48 +113,6 @@ public class DrawView extends View{
             y = dY;
         }
 
-
-        // old version; just in case
-
-        /*if(locations.isEmpty())
-            return;
-        int width = canvas.getWidth();
-        int height = canvas.getHeight();
-
-        double x = (width/360.0) * (180 + locations.get(0).getLongitude());
-        double y = (height/180.0) * (90 - locations.get(0).getLatitude());
-
-        double scaleX = x;
-        double scaleY = y;
-
-        for(int i = 1; i < locations.size(); i++){
-
-            double x2 = (width/360.0) * (180 + locations.get(i).getLongitude());
-            double y2 = (height/180.0) * (90 - locations.get(i).getLatitude());
-
-            double saveX2 = x2;
-            double saveY2 = y2;
-
-
-            if (locations.get(i).getLongitude() > locations.get(i-1).getLongitude())
-                x2 = x2 - ((x2 - scaleX) * 1000000);
-            else
-                x2 = x2 + ((scaleX - x2) * 1000000);
-
-            if (locations.get(i).getLatitude() > locations.get(i-1).getLatitude())
-                y2 = y2 - ((y2 - scaleY) * 1000000);
-            else
-                y2 = y2 + ((scaleY - y2) * 1000000);
-
-            canvas.drawLine((float)x, (float)y, (float)x2, (float)y2, customPaint);
-            x = x2;
-            y = y2;
-
-            scaleX = saveX2;
-            scaleY = saveY2;
-        }*/
     }
-
-
 
 }
